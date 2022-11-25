199211de56055e3725907f01ba3281bc5a7e993b
Wallys Ferreira <105322824+WallysFerreira@users.noreply.github.com>
2022-11-25 20:27:29 +0000

    Validate arguments are in desired range

--- The Audio Programming Book/Chapter 1/nscale.c
+++ The Audio Programming Book/Chapter 1/nscale.c
@@ -1,9 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 int main(int argc, char* argv[]) {
+  int note = atoi(argv[1]), midinote = atoi(argv[2]);
+
   if (argc != 3) {
-    printf("Você precisa dar 2 argumentos notes midinote\n");
+    printf("2 arguments needed: notes and midinote\n");
+    return 1;
+  }
+
+  if (note < 1) {
+    printf("Notes must be positive\n");
+    return 1;
+  } else if (note > 24) {
+    printf("Maximum value for notes is 24\n");
+    return 1;
+  }
+
+  if (midinote < 0) {
+    printf("MIDI notes can't be negative\n");
+    return 1;
+  } else if (midinote > 127) {
+    printf("MIDI notes maximum is 127\n");
     return 1;
   }
 }
