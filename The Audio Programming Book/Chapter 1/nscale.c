2f043588150c4cb662f3fd0a308bbf35f103af69
Wallys Ferreira <105322824+WallysFerreira@users.noreply.github.com>
2022-11-25 20:56:54 +0000

    Finish nscale

--- The Audio Programming Book/Chapter 1/nscale.c
+++ The Audio Programming Book/Chapter 1/nscale.c
@@ -3,17 +3,19 @@
 #include <math.h>
 
 int main(int argc, char* argv[]) {
-  int note = atoi(argv[1]), midinote = atoi(argv[2]);
-
+  int notes = atoi(argv[1]), midinote = atoi(argv[2]);
+  double frequency, ratio, c0, c5;
+  double intervals[24];
+  
   if (argc != 3) {
     printf("2 arguments needed: notes and midinote\n");
     return 1;
   }
 
-  if (note < 1) {
+  if (notes < 1) {
     printf("Notes must be positive\n");
     return 1;
-  } else if (note > 24) {
+  } else if (notes > 24) {
     printf("Maximum value for notes is 24\n");
     return 1;
   }
@@ -24,5 +26,20 @@ int main(int argc, char* argv[]) {
   } else if (midinote > 127) {
     printf("MIDI notes maximum is 127\n");
     return 1;
+  }
+
+  ratio = pow(2.0, 1.0 / 12);
+  c5 = 220 * pow(ratio, 3);
+  c0 = c5 * pow(0.5, 5);
+  frequency = c0 * pow(ratio, midinote);
+
+  ratio = pow(2.0, 1.0 / notes);
+  for (int i = 0; i < notes; i++) {
+    intervals[i] = frequency;
+    frequency *= ratio;
+  }
+
+  for (int i = 0; i < notes; i++) {
+    printf("%f\n", intervals[i]);
   }
 }
