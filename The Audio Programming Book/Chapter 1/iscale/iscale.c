8675a53b834ffcc8349257c4a6d5365f897307f0
Wallys Ferreira <105322824+WallysFerreira@users.noreply.github.com>
2022-11-28 14:20:16 +0000

    Change flag identification to book version

--- The Audio Programming Book/Chapter 1/iscale/iscale.c
+++ The Audio Programming Book/Chapter 1/iscale/iscale.c
@@ -2,21 +2,46 @@
 
 int main(int argc, char* argv[]) {
   int isMidi = 0, writeInterval = 0;
-  argc--;
-  argv++;
 
-  for (int i = 0; i < argc; i++) {
-    if (argv[i][0] == '-') {
-      char flag = argv[i][1];
+  // Find flags anywhere in the command-line
+  // for (int i = 0; i < argc; i++) {
+  //   if (argv[i][0] == '-') {
+  //     char flag = argv[i][1];
+
+  //     if (flag == 'm') {
+  //       isMidi = 1;
+  //     } else if (flag == 'i') {
+  //         writeInterval = 1;
+  //     } else {
+  //       printf("Invalid flag %s\n", argv[i]);
+  //       return 1;
+  //     }
+  //   }
+  // }
+
+  // Find flags in the beginning of command-line arguments (book version)
+  while (argc > 1) {
+    if (argv[1][0] == '-') {
+      char flag = argv[1][1];
 
       if (flag == 'm') {
         isMidi = 1;
+        printf("Teste 1\n");
       } else if (flag == 'i') {
-          writeInterval = 1;
+        writeInterval = 1;
+        printf("Teste 2\n");
       } else {
-        printf("Invalid flag %s\n", argv[i]);
+        printf("Invalid flag %s\n", argv[1]);
         return 1;
       }
+
+      argc--;
+      argv++;
+    } else {
+      break;
     }
   }
+
+  printf("%d\n", argc);
+  printf("%s\n", argv[1]);
 }
