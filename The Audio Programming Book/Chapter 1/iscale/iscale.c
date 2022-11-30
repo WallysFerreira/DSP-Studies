648ed06f1784b2c0d6a2758a172a8d889e2401f1
Wallys Ferreira <105322824+WallysFerreira@users.noreply.github.com>
2022-11-30 18:26:30 +0000

    finish iscale

--- The Audio Programming Book/Chapter 1/iscale/iscale.c
+++ The Audio Programming Book/Chapter 1/iscale/iscale.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main(int argc, char* argv[]) {
-  int isMidi = 0, writeInterval = 0;
+  int isMidi = 0, writeInterval = 0, err = 0, notes;
+  double startVal, baseFreq, ratio, intervals[25];
+  FILE* fp;
 
   // Find flags anywhere in the command-line
   // for (int i = 0; i < argc; i++) {
@@ -26,10 +30,8 @@ int main(int argc, char* argv[]) {
 
       if (flag == 'm') {
         isMidi = 1;
-        printf("Teste 1\n");
       } else if (flag == 'i') {
         writeInterval = 1;
-        printf("Teste 2\n");
       } else {
         printf("Invalid flag %s\n", argv[1]);
         return 1;
@@ -41,6 +43,86 @@ int main(int argc, char* argv[]) {
       break;
     }
   }
+  
+  // There should be 3 arguments now (iscale N startVal), if there isn't, throw an error
+  if (argc < 3) {
+    printf("Arguments missing\n");
+    return 1;
+  }
 
-  // Chapter 1.6.7 150 PDF 123 BOOK
+  // Validate number of notes
+  notes = atoi(argv[1]);
+  if (notes < 1 || notes > 24) {
+    printf("Number of notes must be between 1 and 24\n");
+    return 1;
+  }
+  // Validate start value
+  startVal = atoi(argv[2]);
+  if (isMidi) {
+    if (startVal < 0 || startVal > 127) {
+      printf("MIDI Notes must be between 0 and 127\n");
+      return 1;
+    }
+  } else {
+    if (startVal < 0) {
+      printf("Frequencies must be positive\n");
+      return 1;
+    }
+  }
+
+  // Create new file
+  fp = NULL;
+  if (argc > 3) {
+    fp = fopen(argv[3], "w");
+    if (fp == NULL) {
+      printf("Error creating file\n");
+      perror("");
+    }
+  }
+
+  // Find base frequency with start value being MIDI.
+  if (isMidi) {
+    double c0, c5;
+    ratio = pow(2.0, 1.0 / 12.0);
+    c5 = 220.0 * pow(ratio, 3);
+    c0 = c5 * pow(0.5, 5);
+    baseFreq = c0 * pow(ratio, startVal);
+  } else {
+    baseFreq = startVal;
+  }
+
+  // Calculate ratio for the number of notes and fill array
+  ratio = pow(2.0, 1.0 / notes);
+  for (int i = 0; i <= notes; i++) {
+    intervals[i] = baseFreq;
+    baseFreq *= ratio;
+  }
+
+  // Write array to screen
+  for (int i = 0; i <= notes; i++) {
+    if (writeInterval) {
+      printf("%d:\t%f\t%f\n", i, pow(ratio, i), intervals[i]);
+    } else {
+      printf("%d:\t%f\n", i, intervals[i]);
+    }
+
+    // and to file if needed
+    if (fp) {
+      if(writeInterval) {
+        // if an error occurs, fprintf will return a negative number
+        err = fprintf(fp, "%d:\t%f\t%f\n", i, pow(ratio, i), intervals[i]);
+      } else {
+        err = fprintf(fp, "%d:\t%f\n", i, intervals[i]);
+      }
+
+      if (err < 0) break;
+    }
+  }
+
+  // If err is negative, an error has occurred while writing to file
+  if (err < 0) perror("Error writing to file\n");
+  
+  if (fp) fclose(fp);
+
+  return 0;
 }
